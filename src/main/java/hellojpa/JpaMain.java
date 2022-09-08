d5cc4b543e5121f9f71bce02ad7d42d067c1099b
HiPark <youuy28@gmail.com>
2022-09-08 21:52:00 +0900

    Study persist

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -31,14 +31,44 @@ public class JpaMain {
 //            member.setName("NameChangeTest");
 
             // JPQL
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.name is : " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.name is : " + member.getName());
-            }
+            // Non Persist
+//            Member member = new Member();
+//            member.setId(99L);
+//            member.setName("JPA");
+//
+//            // Persist
+//            System.out.println("=====Before=====");
+//            em.persist(member);
+//            System.out.println("=====After=====");
 
+//            Member findMember1 = em.find(Member.class, 99L);
+//            Member findMember2 = em.find(Member.class, 99L);
+
+//            System.out.println("find member name is : " + findMember.getName());
+//            System.out.println("find member id is   : " + findMember.getId());
+//            System.out.println(" result is  :  " + (findMember1.getName() == findMember2.getName()));
+
+//            Member memberA = new Member(101L, "A");
+//            Member memberB = new Member(102L, "B");
+//
+//            // Not Yet Insert Db
+//            em.persist(memberA);
+//            em.persist(memberB);
+
+            Member member = em.find(Member.class, 101L);
+            member.setName("AAA");
+            
+
+            // DB INSERT
             tx.commit();
+            System.out.println("=====Commit After=====");
         } catch (Exception e) {
             tx.rollback();
         } finally {
