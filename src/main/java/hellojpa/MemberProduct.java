69ca815ff26f771a9e606a510658bba70e21a193
HiPark <youuy28@gmail.com>
2022-09-12 22:24:17 +0900

    N:N -> 1:N

---
+++ src/main/java/hellojpa/MemberProduct.java
@@ -0,0 +1,26 @@
+package hellojpa;
+
+import com.sun.org.apache.xpath.internal.operations.Neg;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    private int count;
+    private int price;
+
+    private LocalDateTime orderDateTime;
+}
