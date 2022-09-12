b2515d12ebdd497f9d4d2cc09667fe884344dad1
HiPark <youuy28@gmail.com>
2022-09-12 21:49:23 +0900

    1:N

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,121 +18,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // Insert
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
-//            em.persist(member);
-
-            // Update
-//            Member member = em.find(Member.class, 1L);
-//            System.out.println("find member name is : " + member.getName());
-//            System.out.println("find member id is   : " + member.getId());
-//            member.setName("NameChangeTest");
-
-            // JPQL
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .getResultList();
-//
-//            for (Member member : result) {
-//                System.out.println("member.name is : " + member.getName());
-//            }
-
-            // Non Persist
-//            Member member = new Member();
-//            member.setId(99L);
-//            member.setName("JPA");
-//
-//            // Persist
-//            System.out.println("=====Before=====");
-//            em.persist(member);
-//            System.out.println("=====After=====");
-
-//            Member findMember1 = em.find(Member.class, 99L);
-//            Member findMember2 = em.find(Member.class, 99L);
-
-//            System.out.println("find member name is : " + findMember.getName());
-//            System.out.println("find member id is   : " + findMember.getId());
-//            System.out.println(" result is  :  " + (findMember1.getName() == findMember2.getName()));
-
-//            Member memberA = new Member(101L, "A");
-//            Member memberB = new Member(102L, "B");
-//
-//            // Not Yet Insert Db
-//            em.persist(memberA);
-//            em.persist(memberB);
-
-//            Member member = em.find(Member.class, 101L);
-//            member.setName("AAA");
-
-//            Member member1 = new Member();
-//            member1.setUsername("A");
-//
-//            Member member2 = new Member();
-//            member2.setUsername("B");
-//
-//            Member member3 = new Member();
-//            member3.setUsername("C");
-//
-//            System.out.println("==================================");
-//            em.persist(member1);
-//            em.persist(member2);
-//            em.persist(member3);
-//
-//            System.out.println("member1 = " + member1.getId());
-//            System.out.println("member2 = " + member2.getId());
-//            System.out.println("member3 = " + member3.getId());
-//
-//            System.out.println("==================================");
-//
-
-//            // one-way association
-//            // Save
-//            Team team = new Team();
-//            team.setName("TeamA");
-//            em.persist(team);
-//
-//            Member member = new Member();
-//            member.setUsername("member1");
-//            member.setTeam(team);
-//            em.persist(member);
-//
-//            em.flush();
-//            em.clear();
-//
-//            // SEARCH
-//            Member findMember = em.find(Member.class, member.getId());
-//            Team findTeam = findMember.getTeam();
-//            System.out.println("find Team is : " + findTeam.getName());
-//
-//            // Team Changed
-////            Team newTeam = em.find(Team.class, 100L); // Need 100L Team
-////            findMember.setTeam(newTeam);
-
-            // two-way association
-            // Save
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
             Member member = new Member();
             member.setUsername("member1");
-            member.changeTeam(team);
+
             em.persist(member);
 
-            team.addMember(member);
+            Team team = new Team();
+            team.setName("teamA");
+            team.getMembers().add(member);
 
-            em.flush();
-            em.clear();
-
-            // SEARCH
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
-                System.out.println("m : " + m.getUsername());
-
-            }
+            em.persist(team);
 
             // DB INSERT
             tx.commit();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,11 +4,6 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-//@TableGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        table = "MY_SEQUENCES",
-//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -17,24 +12,9 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(name = "TEAM_ID",insertable = false, updatable = false)
     private Team team;
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void changeTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 
     public Long getId() {
         return id;
@@ -52,95 +32,5 @@ public class Member {
         this.username = username;
     }
 
-//    public Long getTeamId() {
-//        return teamId;
-//    }
-//
-//    public void setTeamId(Long teamId) {
-//        this.teamId = teamId;
-//    }
 
-
-
-
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.TABLE,
-//            generator = "MEMBER_SEQ_GENERATOR")
-//    private Long id;
-//    @Column(name = "name", nullable = false, length = 10)
-//    private String username;
-//    private Integer age;
-//
-//    @Enumerated(EnumType.STRING)
-//    private RoleType roleType;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createDate;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date lastModifiedDate;
-//
-//    @Lob
-//    private String description;
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public Integer getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Integer age) {
-//        this.age = age;
-//    }
-//
-//    public RoleType getRoleType() {
-//        return roleType;
-//    }
-//
-//    public void setRoleType(RoleType roleType) {
-//        this.roleType = roleType;
-//    }
-//
-//    public Date getCreateDate() {
-//        return createDate;
-//    }
-//
-//    public void setCreateDate(Date createDate) {
-//        this.createDate = createDate;
-//    }
-//
-//    public Date getLastModifiedDate() {
-//        return lastModifiedDate;
-//    }
-//
-//    public void setLastModifiedDate(Date lastModifiedDate) {
-//        this.lastModifiedDate = lastModifiedDate;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public Member() {
-//
-//    }
 }
